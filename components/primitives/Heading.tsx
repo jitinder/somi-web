6895d7257cc07b71f442c9363e33f1552677361d
Sidak <sid@msquared.io>
2026-02-03 20:39:18 +0000

    fix build

--- components/primitives/Heading.tsx
+++ components/primitives/Heading.tsx
@@ -11,7 +11,22 @@ const defaultStyles: Record<number, string> = {
 };
 
 export function Heading({ children, className, level = 1 }: HeadingProps) {
-  const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+  const classes = cn(defaultStyles[level], className);
 
-  return <Tag className={cn(defaultStyles[level], className)}>{children}</Tag>;
+  switch (level) {
+    case 1:
+      return <h1 className={classes}>{children}</h1>;
+    case 2:
+      return <h2 className={classes}>{children}</h2>;
+    case 3:
+      return <h3 className={classes}>{children}</h3>;
+    case 4:
+      return <h4 className={classes}>{children}</h4>;
+    case 5:
+      return <h5 className={classes}>{children}</h5>;
+    case 6:
+      return <h6 className={classes}>{children}</h6>;
+    default:
+      return <h1 className={classes}>{children}</h1>;
+  }
 }
